79ee79a28732540db7b41c6bedcf040d6de3d86c
BaselWaheed <baselwaheed66@gmail.com>
2025-08-02 15:20:22 +0000

    last Update

---
+++ test_one/test_one/doctype/test_1/test_test_1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Basel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testtest1(FrappeTestCase):
+	pass
---
+++ test_one/test_one/doctype/test_2/test_2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Basel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("test 2", {
+// 	refresh(frm) {
+
+// 	},
+// });
